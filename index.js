bb91ca9768050479b42d4d4b364d114e9d365e9a
Mathijs van den Doel <mathijs@192.168.2.34>
2021-06-17 19:57:34 +0200

    fourth

--- index.js
+++ index.js
@@ -7,18 +7,20 @@ const app = Express();
 const port = 3000; 
 
 app.use(Express.static("public"));
+app.set('view-engine', 'ejs')
+
+let loggedIn = [];
+// {"username": "", "IP": ""}
 
 app.get("/", (req, res)=> {
-    res.sendFile(__dirname + "/public/index.html");
+    res.render(__dirname + "/public/index.ejs", {name: "mattydoel"});
 })
-app.use(bodyParser.urlencoded({ extended: false }))
 
-// parse application/json
-// app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 
 app.post('/login', (req, res) => {
-    console.log(req.socket.remoteAddress);
+    // console.log("User request with IP-address: $(req.socket.remoteAddress)");
     const username = req.body.username;
     const password = req.body.password;
     // console.log(username);
@@ -29,6 +31,8 @@ app.post('/login', (req, res) => {
 
     for(let i = 0;i<users.length;i++){
         if(username == users[i]["username"] && password == users[i]["password"]){
+            loggedIn.push({"username": username, "IP": req.socket.remoteAddress})
+            console.log(loggedIn);
             res.redirect("/dashboard");
             break;
         }
@@ -39,9 +43,38 @@ app.post('/login', (req, res) => {
   });
 
 app.get("/dashboard", (req, res)=> {
-    res.send("received your request");
+    console.log(loggedIn);
+    if(loggedIn.length == 0){
+        res.redirect("/")
+    }
+    else{
+        for(let i = 0;i<loggedIn.length;i++){
+            if(req.socket.remoteAddress == loggedIn[i]["IP"]){
+                res.sendFile(__dirname + "/public/dashboard.html");
+                break;
+            }
+            else if(i == loggedIn.length - 1){
+                res.redirect("/")
+            }
+        }
+    }
+})
+
+app.get("/quiz/:index", (req, res)=>{
+    let index = parseInt(req.params.index);
+    let rawdata = fs.readFileSync('public/questions.json');
+    let temp_data = JSON.parse(rawdata);
+    let data = temp_data["questions"];
+    if(index >= data.length){
+        res.send("question not found");
+    }
+    else{
+        let next_index = index + 1;
+        let previous_index = index - 1;
+        res.render(__dirname + "/quiz.ejs", {main_question: data[index]["question"], a:data[index]["answers"][0], b:data[index]["answers"][1], c:data[index]["answers"][2], d:data[index]["answers"][3], next:next_index, previous:previous_index});
+    }
 })
 
 app.listen(process.env.PORT || port, ()=>{
-    console.log("server is running...");
+    console.log("server is running.......");
 })
