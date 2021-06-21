4c0027b577859b95b6f757c9f240de6ab7232756
Mathijs van den Doel <mathijs@192.168.2.34>
2021-06-21 19:49:20 +0200

    Home page added

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post('/login', (req, res) => {
     for(let i = 0;i<users.length;i++){
         if(username == users[i]["username"] && password == users[i]["password"]){
             loggedIn.push({"username": username, "IP": req.socket.remoteAddress})
-            console.log(loggedIn);
+            // console.log(loggedIn);
             res.redirect("/dashboard");
             break;
         }
@@ -50,7 +50,7 @@ app.get("/dashboard", (req, res)=> {
     else{
         for(let i = 0;i<loggedIn.length;i++){
             if(req.socket.remoteAddress == loggedIn[i]["IP"]){
-                res.sendFile(__dirname + "/public/dashboard.html");
+                res.render(__dirname + "/public/dashboard.ejs");
                 break;
             }
             else if(i == loggedIn.length - 1){
@@ -71,8 +71,83 @@ app.get("/quiz/:index", (req, res)=>{
     else{
         let next_index = index + 1;
         let previous_index = index - 1;
-        res.render(__dirname + "/quiz.ejs", {main_question: data[index]["question"], a:data[index]["answers"][0], b:data[index]["answers"][1], c:data[index]["answers"][2], d:data[index]["answers"][3], next:next_index, previous:previous_index});
+        let total_questions = data.length;
+        res.render(__dirname + "/quiz.ejs", {main_question: data[index]["question"], a:data[index]["answers"][0], b:data[index]["answers"][1], c:data[index]["answers"][2], d:data[index]["answers"][3], e:data[index]["answers"][4], next:next_index, previous:previous_index, total_questions:total_questions, index:index});
     }
+})
+
+app.post("/quiz/:index", (req, res)=>{
+    let index = parseInt(req.params.index);
+    let index_str = req.params.index;
+    let answers = [];
+    const value = req.body.value;
+    answers.push({"question": index_str, "answer": value});
+
+    let rawdata = fs.readFileSync('courses-results.json');
+    let data = JSON.parse(rawdata);
+    let users = data["users"];
+    let user;
+    for(let i = 0;i < loggedIn.length;i++){
+        if(loggedIn[i]["IP"] == req.socket.remoteAddress){
+            user = loggedIn[i]["username"];
+        }
+    }
+    let user_info = users[user];
+    let course = user_info["course1"];
+    if(course.length == 0){
+        course.push({"question": index_str, "answer": value});
+    }
+    else{
+        for(let i = 0;i<course.length;i++){
+            if(course[i]["question"] == index_str){
+                course[i]["answer"] = value;
+                break;
+            }
+            else if(i == course.length - 1){
+                course.push({"question": index_str, "answer": value});
+            }
+        }
+    }
+    data["users"][user]["course1"] = course;
+    let json_data = JSON.stringify(data, null, 2);
+    fs.writeFileSync("courses-results.json", json_data);
+
+    res.redirect("/quiz/" + index);
+});
+
+app.get("/results/:index", (req, res)=>{
+    let index = parseInt(req.params.index);
+    let user;
+    for(let i = 0;i < loggedIn.length;i++){
+        if(loggedIn[i]["IP"] == req.socket.remoteAddress){
+            user = loggedIn[i]["username"];
+        }
+    }
+    let rawdata_results = fs.readFileSync('courses-results.json');
+    let data_results = JSON.parse(rawdata_results);
+    let users_resuslts = data_results["users"];
+
+    let rawdata_questions = fs.readFileSync('public/question.json');
+    let data_questions = JSON.parse(rawdata_questions);
+    let users_questions = data_questions["users"];
+
+
+    res.render(__dirname + "/public/results.ejs")
+})
+
+app.get("/test", (req, res)=>{
+    things = ["hoi", "hallo", "hi"];
+    res.render(__dirname + "/public/test.ejs", things=things)
+})
+
+app.post("/test", (req, res)=>{
+    const value = req.body.value;
+    console.log(value);
+    res.redirect("/test")
+})
+
+app.get("/home", (req, res)=>{
+    res.render(__dirname + "/public/home.ejs")
 })
 
 app.listen(process.env.PORT || port, ()=>{
